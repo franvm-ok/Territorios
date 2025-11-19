b154e6db670b5a37be3f3efca428ed5fb4acaf14
franvm-ok <franvmok@gmail.com>
2025-11-19 18:42:18 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function openTerritorio(numero) {
-  alert("Abrir Territorio " + numero);
-
-  // Acá podés redirigir a páginas por cada territorio:
-  // window.location.href = "territorio" + numero + ".html";
-}
