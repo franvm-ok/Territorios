fb4e4d7caeb8c2df2188c9454c0d41c0492ef4db
franvm-ok <franvmok@gmail.com>
2025-11-24 19:15:34 -0300

    Actualizar firebase.js

--- firebase.js
+++ firebase.js
@@ -1,26 +1,28 @@
-// CONFIG DE TU FIREBASE
+// firebase.js (para toda la app)
+
+// Importar Firebase (V12.6.0)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
+import { 
+    getDatabase, ref, set, push, update, onValue 
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js";
+
+// Configuración del proyecto
 const firebaseConfig = {
-  apiKey: "AIzaSyAbAnz0hAHwttGtoDPjSzSgiJ9HP1wz-YY",
-  authDomain: "territorio-3c28d.firebaseapp.com",
-  databaseURL: "https://territorio-3c28d-default-rtdb.firebaseio.com/",
-  projectId: "territorio-3c28d",
-  storageBucket: "territorio-3c28d.appspot.com",
-  messagingSenderId: "369901367983",
-  appId: "1:369901367983:web:33237b1b5661b82023a763"
+    apiKey: "AIzaSyBQdOIpv75AblS5i61eIq2YBBDEO-YfVuk",
+    authDomain: "territorio-3c28d.firebaseapp.com",
+    databaseURL: "https://territorio-3c28d-default-rtdb.firebaseio.com",
+    projectId: "territorio-3c28d",
+    storageBucket: "territorio-3c28d.firebasestorage.app",
+    messagingSenderId: "170925082008",
+    appId: "1:170925082008:web:edccd36a72b5b2d3bd57ed",
+    measurementId: "G-ZL63C8VN36"
 };
 
-// INICIALIZA FIREBASE
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
+// Inicializar
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getDatabase(app);
 
-// Guarda un reporte
-function guardarReporte(data) {
-  return db.ref("reportes").push(data);
-}
-
-// Escucha reportes en tiempo real
-function escucharReportes(callback) {
-  db.ref("reportes").on("value", snapshot => {
-    callback(snapshot.val());
-  });
-}
+// Exportar funciones útiles
+export { ref, set, push, update, onValue };
